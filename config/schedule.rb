988a902358b4042d8e0db899a6739d9b77669340
James Mead <james@floehopper.org>
2014-04-21 15:28:03 +0100

    Another Ruby 1.8.7 fix.

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,6 +1,6 @@
 env :PATH, '/usr/local/bin:/usr/bin:/bin'
 env :MAILTO, 'everyone@gofreerange.com'
 
-every :day, at: '2am' do
+every :day, :at => '2am' do
   command 'cd ~/app && ruby backup.rb'
 end
