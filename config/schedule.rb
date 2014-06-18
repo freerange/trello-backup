69cbf24656f8f9ef5bbdb2a66058f89c1842b212
James Mead <james@floehopper.org>
2014-06-18 11:04:49 +0100

    Make cron job quieter.
    This redirects all output to a log file, but echoes an error message to stdout
    if the Ruby script exit code indicates an error. This should mean we'll only
    see an email if the cron job fails.

--- config/schedule.rb
+++ config/schedule.rb
@@ -2,5 +2,5 @@ env :PATH, '/usr/local/bin:/usr/bin:/bin'
 env :MAILTO, 'everyone@gofreerange.com'
 
 every :day, :at => '2am' do
-  command 'cd ~/app && ruby backup.rb'
+  command 'cd ~/app && ruby backup.rb >> trello_backup.log 2>&1 || echo "Trello Backup: error occurred. See trello_backup.log for details."'
 end
