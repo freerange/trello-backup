2e6e8c957bf76f3bd51ad366650711e468f6fb59
James Mead <3169+floehopper@users.noreply.github.com>
2018-12-15 15:05:20 +0000

    Write Trello board data to S3 bucket
    I've chosen to use the board ID rather than a normalized version of the
    board name for the S3 object key, because this is a lot less likely to
    change over time.
    
    There's no need to include a timestamp in the S3 object key, because the
    S3 bucket has versioning enabled.

--- lambdaFunctions/backupTrelloBoard/index.rb
+++ lambdaFunctions/backupTrelloBoard/index.rb
@@ -1,8 +1,11 @@
 require 'addressable/uri'
+require 'aws-sdk-s3'
 require 'dotenv'
 require 'restclient'
 
 Dotenv.load
+
+S3_BUCKET_NAME = ENV.fetch('BACKUP_TRELLO_BOARD_S3_BUCKET_NAME')
 
 def handler(event:, context:)
   records = event.fetch('Records', [])
@@ -31,6 +34,11 @@ def handler(event:, context:)
     puts "#{board_id} - Fetching Trello data for #{board_name} board..."
     response = RestClient.get(uri.to_s)
     json = response.body
+    puts "#{board_id} - OK"
+
+    puts "#{board_id} - Writing data for #{board_name} board to S3..."
+    object = Aws::S3::Object.new(S3_BUCKET_NAME, "#{board_id}.json")
+    object.upload_stream(content_type: 'application/json') { |stream| stream << json }
     puts "#{board_id} - OK"
   end
 
