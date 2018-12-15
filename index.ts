a7b25e050b47c08f00136ac14858bac99020ef1e
James Mead <3169+floehopper@users.noreply.github.com>
2018-12-15 11:35:39 +0000

    Create an EventRule to fire at daily at 2am
    This rule currently has no target and so won't do anything, but it's a
    small step on the way.

--- index.ts
+++ index.ts
@@ -1,8 +1,14 @@
 import cdk = require('@aws-cdk/cdk');
+import events = require('@aws-cdk/aws-events');
 
 class TrelloBackupStack extends cdk.Stack {
   constructor(parent: cdk.App, id: string, props?: cdk.StackProps) {
     super(parent, id, props);
+
+    const dailyAt2am = 'cron(0 2 * * ? *)';
+    const dailyAt2amRule = new events.EventRule(this, 'DailyAt2amRule', {
+       scheduleExpression: dailyAt2am,
+    });
   }
 }
 
