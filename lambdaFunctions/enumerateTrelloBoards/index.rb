52b61f1c7827bdbbb065589ff94eecaca2080423
James Mead <3169+floehopper@users.noreply.github.com>
2018-12-15 13:56:33 +0000

    Use trello gem to print name of first board
    Note that unfortunately the trello gem currently (rather unnecessarily)
    has an indirect dependency on the unf gem which has native extensions.
    When I was trying this previously, I thought I ran into problems because
    the extensions were being built on my local machine using Mac OSX
    (x86_64-darwin-17), but were being deployed and run in an AWS Lambda
    function which is an x86_64-linux platform. Previously I had used Docker
    to build the extensions for the correct platform, however, when I just
    tried deploying and running the function as it stands (with Mac
    OSX-build extensions), it seemed to work OK, so maybe the whole Docker
    thing was unnecessary!

--- lambdaFunctions/enumerateTrelloBoards/index.rb
+++ lambdaFunctions/enumerateTrelloBoards/index.rb
@@ -1,10 +1,14 @@
 require 'dotenv'
+require 'trello'
 
 Dotenv.load
 
-p ENV.fetch('TRELLO_KEY')
-p ENV.fetch('TRELLO_TOKEN')
+Trello.configure do |config|
+  config.developer_public_key = ENV.fetch('TRELLO_KEY')
+  config.member_token = ENV.fetch('TRELLO_TOKEN')
+end
 
 def handler(event:, context:)
+  p Trello::Board.all.first.name
   { statusCode: 200, body: 'OK' }
 end
