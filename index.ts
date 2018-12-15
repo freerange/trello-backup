02f10abe765718126e136d2f9a6e4c9984bc2d3d
James Mead <3169+floehopper@users.noreply.github.com>
2018-12-15 15:04:44 +0000

    Create a versioned S3 bucket & allow backupTrelloBoard to write to it

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import cdk = require('@aws-cdk/cdk');
 import lambda = require('@aws-cdk/aws-lambda');
 import events = require('@aws-cdk/aws-events');
+import s3 = require('@aws-cdk/aws-s3');
 import { Topic } from '@aws-cdk/aws-sns';
 
 class TrelloBackupStack extends cdk.Stack {
@@ -19,12 +20,22 @@ class TrelloBackupStack extends cdk.Stack {
       timeout: 30
     });
 
+    const trelloBoardBackupsBucket = new s3.Bucket(this, 'trelloBoardBackupsBucket', {
+      bucketName: 'trello-board-backups.gofreerange.com',
+      versioned: true
+    });
+
     const backupTrelloBoard = new lambda.Function(this, 'backupTrelloBoard', {
       runtime: new lambda.Runtime('ruby2.5'),
       handler: 'index.handler',
       code: lambda.Code.asset('./lambdaFunctions/backupTrelloBoard'),
+      environment: {
+        BACKUP_TRELLO_BOARD_S3_BUCKET_NAME: trelloBoardBackupsBucket.bucketName
+      },
       timeout: 30
     });
+
+    trelloBoardBackupsBucket.grantPut(backupTrelloBoard.role);
 
     backupTrelloBoardTopic.grantPublish(enumerateTrelloBoards.role);
     backupTrelloBoardTopic.subscribeLambda(backupTrelloBoard);
