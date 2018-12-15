03513a1491d22678c8f7fd0a7febaf14562b2ca8
James Mead <3169+floehopper@users.noreply.github.com>
2018-12-15 14:23:12 +0000

    Create SNS Topic and publish message to it for each Trello board

--- index.ts
+++ index.ts
@@ -1,17 +1,25 @@
 import cdk = require('@aws-cdk/cdk');
 import lambda = require('@aws-cdk/aws-lambda');
 import events = require('@aws-cdk/aws-events');
+import { Topic } from '@aws-cdk/aws-sns';
 
 class TrelloBackupStack extends cdk.Stack {
   constructor(parent: cdk.App, id: string, props?: cdk.StackProps) {
     super(parent, id, props);
 
+    const backupTrelloBoardTopic = new Topic(this, 'backupTrelloBoardTopic');
+
     const enumerateTrelloBoards = new lambda.Function(this, 'enumerateTrelloBoards', {
       runtime: new lambda.Runtime('ruby2.5'),
       handler: 'index.handler',
       code: lambda.Code.asset('./lambdaFunctions/enumerateTrelloBoards'),
+      environment: {
+        BACKUP_TRELLO_BOARD_TOPIC_ARN: backupTrelloBoardTopic.topicArn
+      },
       timeout: 30
     });
+
+    backupTrelloBoardTopic.grantPublish(enumerateTrelloBoards.role);
 
     const dailyAt2am = 'cron(0 2 * * ? *)';
     const dailyAt2amRule = new events.EventRule(this, 'DailyAt2amRule', {
--- lambdaFunctions/enumerateTrelloBoards/index.rb
+++ lambdaFunctions/enumerateTrelloBoards/index.rb
@@ -1,3 +1,4 @@
+require 'aws-sdk-sns'
 require 'dotenv'
 require 'trello'
 
@@ -8,7 +9,27 @@ Trello.configure do |config|
   config.member_token = ENV.fetch('TRELLO_TOKEN')
 end
 
+TOPIC_ARN = ENV.fetch('BACKUP_TRELLO_BOARD_TOPIC_ARN')
+
 def handler(event:, context:)
-  p Trello::Board.all.first.name
+  sns = Aws::SNS::Client.new
+  boards = Trello::Board.all
+  boards.each do |board|
+    sns.publish(
+      topic_arn: TOPIC_ARN,
+      subject: "Backup Trello board",
+      message: "Backup Trello board: #{board.name}",
+      message_attributes: {
+        'board_id' => {
+          data_type: 'String',
+          string_value: board.id
+        },
+        'board_name' => {
+          data_type: 'String',
+          string_value: board.name
+        }
+      }
+    )
+  end
   { statusCode: 200, body: 'OK' }
 end
