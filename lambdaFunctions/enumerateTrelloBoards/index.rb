9693ccd070cd5fec308432ed840d23f6adb2a0bb
James Mead <3169+floehopper@users.noreply.github.com>
2018-12-23 22:11:23 +0000

    Avoid using ruby-trello gem to reduce dependencies
    And reduce the size of the zip file uploaded to AWS.

--- lambdaFunctions/enumerateTrelloBoards/index.rb
+++ lambdaFunctions/enumerateTrelloBoards/index.rb
@@ -1,32 +1,39 @@
+require 'addressable/uri'
 require 'aws-sdk-sns'
 require 'dotenv'
-require 'trello'
+require 'restclient'
 
 Dotenv.load
-
-Trello.configure do |config|
-  config.developer_public_key = ENV.fetch('TRELLO_KEY')
-  config.member_token = ENV.fetch('TRELLO_TOKEN')
-end
 
 TOPIC_ARN = ENV.fetch('BACKUP_TRELLO_BOARD_TOPIC_ARN')
 
 def handler(event:, context:)
   sns = Aws::SNS::Client.new
-  boards = Trello::Board.all
+
+  endpoint = "https://api.trello.com/1/members/me/boards"
+  uri = Addressable::URI.parse(endpoint)
+  uri.query_values = {
+    :key => ENV.fetch('TRELLO_KEY'),
+    :token => ENV.fetch('TRELLO_TOKEN')
+  }
+  response = RestClient.get(uri.to_s)
+  boards = JSON.parse(response.body)
+
   boards.each do |board|
+    board_id = board['id']
+    board_name = board['name']
     sns.publish(
       topic_arn: TOPIC_ARN,
       subject: "Backup Trello board",
-      message: "Backup Trello board: #{board.name}",
+      message: "Backup Trello board: #{board_name}",
       message_attributes: {
         'board_id' => {
           data_type: 'String',
-          string_value: board.id
+          string_value: board_id
         },
         'board_name' => {
           data_type: 'String',
-          string_value: board.name
+          string_value: board_name
         }
       }
     )
